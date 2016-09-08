c5cc939a85313b2b333059b0befee1df9c6ca8b8
Jatin K Malik <jk.malik@yahoo.com>
2016-09-08 03:16:16 +0000

    Calculate Pi upto decimal digits entered

---
+++ numbers/piToNthDigit.py
@@ -0,0 +1,17 @@
+from decimal import *
+
+print("\t\tPI Calculator")
+
+while(1):
+    length = int(input("\nEnter a number to generate PI up to that many decimal places : "))
+    getcontext().prec = length+1
+
+    if(length<100):
+        break
+
+    else:
+        print("\nNumber should be less than 100")
+
+print("Result is : "+str(Decimal(355)/Decimal(113)))
+
+# I know this is not the best way to calculate the value of Pi, I will revisit this after some research
