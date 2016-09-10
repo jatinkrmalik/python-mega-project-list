d630707fbe76f1f4e7e05fd794c749bcdec53137
Jatin K Malik <jk.malik@yahoo.com>
2016-09-10 07:53:50 +0000

    Fibonacci initial checkin

---
+++ numbers/fibonacci.py
@@ -0,0 +1,12 @@
+# Enter a number and have the program generate the Fibonacci sequence to that number or to the Nth number.
+
+def fibo(n):
+    
+
+
+
+print("\n\t\tFibonacci sequence\n")
+
+length = int(input("\nEnter a number to generate Fibonacci series upto that length : "))
+print("\nFibonacci series upto "+str(length)+" length is :\n\n"+fibo(length)
+
