dfca5c0d711a509102f594904c25b1b51a331b41
Jatin K Malik <jk.malik@yahoo.com>
2016-09-12 22:18:12 +0000

    Init Mortgage + Calculator

---
+++ numbers/mortgageCalculator.py
@@ -0,0 +1,6 @@
+# Mortgage Calculator
+# Calculate the monthly payments of a fixed term mortgage over given Nth terms at a given interest rate. Also figure out how long it will take the user to pay back the loan. For added complexity, add an option for users to select the compounding interval (Monthly, Weekly, Daily, Continually).
+
+
+
+
