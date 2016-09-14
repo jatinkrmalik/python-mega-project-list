b4f6bc44aacc14a38c9de66a8de1d42411052737
Jatin K Malik <jk.malik@yahoo.com>
2016-09-14 18:19:15 +0000

    Init Scientific calculator

--- numbers/scientificCalculator.py
+++ numbers/scientificCalculator.py
@@ -1,2 +1,17 @@
 # Calculator
 # A simple calculator to do basic operators. Make it a scientific calculator for added complexity.
+
+while True:
+    print("\n\t\tCalculaor\n\n(A)dd\n(S)ubtract\n(M)ultiply\n(D)ivide\n(Q) Quit")
+    choice =input(">>> ").lower().rstrip()
+    if choice=="q":
+        break
+    elif choice=="a":
+        # Add code
+    elif choice=="s":
+        # Sub code
+    else:
+        print("Invalid choice, please choose again!\n")
+
+print("Thank you")
+
