678cb750d448eecb6a7aa42d7a1032239dceecab
Jatin K Malik <jk.malik@yahoo.com>
2016-09-14 18:25:18 +0000

    Identifying calculator functions

--- numbers/scientificCalculator.py
+++ numbers/scientificCalculator.py
@@ -2,7 +2,7 @@
 # A simple calculator to do basic operators. Make it a scientific calculator for added complexity.
 
 while True:
-    print("\n\t\tCalculaor\n\n(A)dd\n(S)ubtract\n(M)ultiply\n(D)ivide\n(Q) Quit")
+    print("\n\t\tCalculaor\n\n(A)dd\n(S)ubtract\n(M)ultiply\n(D)ivide\n(Mo)dulus\n(Sq)uare\n(Cu)be\n(Si)n\n(Co)s\n(Ta)n\n(P)ower\n(Q)uit")
     choice =input(">>> ").lower().rstrip()
     if choice=="q":
         break
@@ -10,6 +10,24 @@ while True:
         # Add code
     elif choice=="s":
         # Sub code
+    elif choice=="m":
+        # Multiply code
+    elif choice=="d":
+        # Divide code
+    elif choice=="mo":
+        # Modulus code
+    elif choice=="sq":
+        # Square code
+    elif choice=="cu":
+        # Cube code
+    elif choice=="si":
+        # Sine code
+    elif choice=="co":
+        # Cosine code
+    elif choice=="ta":
+        # Tangent code
+    elif choice=="p":
+        # Power code
     else:
         print("Invalid choice, please choose again!\n")
 
