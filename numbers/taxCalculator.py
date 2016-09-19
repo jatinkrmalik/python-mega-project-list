ec7477d48b75f5b52dbff3d5153b5d78ffa19c8f
Jatin K Malik <jk.malik@yahoo.com>
2016-09-19 18:09:18 +0000

    Optimising the tax calculator program to show indi tax

--- numbers/taxCalculator.py
+++ numbers/taxCalculator.py
@@ -2,11 +2,15 @@
 # Asks the user to enter a cost and state tax. It then returns the tax plus the total cost with tax.
 
 def calcTax(cost,tax):
-    return cost+(cost*tax/100)
+    taxC = cost*(tax/100) # calculating tax
+    return taxC,cost+taxC # returning both tax and total amount
 
 print("\n\t\tTax Calculator\n")
 cost = int(input("\nPlease enter the cost of the item: INR "))
 tax = float(input("\nPlease enter the tax (in %): "))
 
-print("\n>> Total amount is: INR "+str(calcTax(cost,tax)))
+print("\n>>> Tax calucated on the item is: INR "+str(calcTax(cost,tax)[0]))
+print("\n>>> Total amount is: INR "+str(calcTax(cost,tax)[1]))
 print() # because asthetics!
+
+
