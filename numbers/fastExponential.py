50ffda85458588177819ea973069c379ec4f6bff
Jatin K Malik <jk.malik@yahoo.com>
2016-09-21 18:56:02 +0000

    Fast exponential partial checkin

---
+++ numbers/fastExponential.py
@@ -0,0 +1,15 @@
+# Fast Exponential
+# Ask the user to enter 2 integers a and b and output a^b (i.e. pow(a,b)) in O(lg n) time complexity.
+
+def fastExp(base, power):
+    # Write magic here
+
+
+while(True):
+    print("\n\t\tWelcome to Fast Exponential\n")
+    base = int(input("Enter the base number: "))
+    power = int(input("Enter the exponential power: "))
+    fastExp(base, power)
+    input("\nPress enter to continue...")
+
+    
