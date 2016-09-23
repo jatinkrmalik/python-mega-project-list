43ae8ad64b06e6675b0afa50d2266f760e92690f
Jatin K Malik <jk.malik@yahoo.com>
2016-09-23 06:51:21 +0000

    Init Number names - spell an input number

---
+++ numbers/numberNames.py
@@ -0,0 +1,11 @@
+# Number Names
+# Show how to spell out a number in English. You can use a preexisting implementation or roll your own, but you should support inputs up to at least one million
+
+def spellMe(num):
+    # Do magic here
+
+while(True):
+    print("\n\t\tWelcome to Number names\n")
+    num = int(input("Enter the number to name: "))
+    print("\nYour answer is: "+str(spellMe(num)))
+    input("\nPress enter to continue...")
