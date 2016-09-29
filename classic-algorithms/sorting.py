bf8e6b937c66241b95b87c86de7468e54872e4c1
Jatin K Malik <jk.malik@yahoo.com>
2016-09-29 14:09:20 +0000

    Implementing bubble sort

--- classic-algorithms/sorting.py
+++ classic-algorithms/sorting.py
@@ -2,10 +2,26 @@
 # Implement two types of sorting algorithms: Merge sort and bubble sort.
 
 def mergeSort(numList):
-    # Do the magic here
-
+    print("Do the magic here")
+    
 def bubbleSort(numList):
     # Do the magic here
+    ps = 0
+    while(True):
+        swpC = 0
+        for i in range(len(numList)-1):
+            j = i + 1
+            if numList[j]<numList[i]:
+                numList[i], numList[j] = numList[j], numList[i]
+                swpC += 1
+        ps += 1
+        print("\n>>>After "+str(ps)+" passes, list is :\n")
+        print(numList)
+
+        if swpC == 0:
+            print("\n***List is now sorted!***")
+            break
+    return numList
 
 while(True):
     print("\n\t\tSorting")
@@ -16,11 +32,13 @@ while(True):
         mergeSort(numList)
 
     elif ch == 'b':
-        bubbleSort(numList)
+        print(bubbleSort(numList))
 
     elif ch == 'q':
         break
 
-    else
+    else:
         print("\nWrong choice entered, please try again.")
+
+    input("\nPress enter to continue...")
 
