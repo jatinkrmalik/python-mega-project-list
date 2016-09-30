7efde738010aed87b51f096f03425a27be563275
Jatin K Malik <jk.malik@yahoo.com>
2016-09-30 18:14:06 +0000

    Implemeting merge sort

--- classic-algorithms/sorting.py
+++ classic-algorithms/sorting.py
@@ -2,8 +2,38 @@
 # Implement two types of sorting algorithms: Merge sort and bubble sort.
 
 def mergeSort(numList):
-    print("Do the magic here")
-    
+    if len(numList)>1:
+        mid = len(numList)//2
+        leftList = numList[:mid]
+        rightList = numList[mid:]
+
+        mergeSort(leftList)
+        mergeSort(rightList)
+
+        i = 0
+        j = 0
+        k = 0
+
+        while i<len(leftList) and j <len(rightList):
+            if leftList[i]<rightList[j]:
+                numList[k] = leftList[i]
+                i += 1
+            else:
+                numList[k] = rightList[j]
+                j += 1
+            k += 1
+
+        while i<len(leftList):
+            numList[k] = leftList[i]
+            i += 1
+            k += 1
+
+        while j<len(rightList):
+            numList[k] = rightList[j]
+            j += 1
+            k += 1
+        return numList
+
 def bubbleSort(numList):
     # Do the magic here
     ps = 0
@@ -15,21 +45,22 @@ def bubbleSort(numList):
                 numList[i], numList[j] = numList[j], numList[i]
                 swpC += 1
         ps += 1
-        print("\n>>>After "+str(ps)+" passes, list is :\n")
+        print("\n>>>After "+str(ps)+" passes, list is :")
         print(numList)
 
         if swpC == 0:
-            print("\n***List is now sorted!***")
+            print("\n***List is now sorted in "+str(ps)+" passes!***")
             break
     return numList
 
 while(True):
     print("\n\t\tSorting")
     numList = input("Please enter the list of numbers seprated by comma ',': ").split(",")
-    ch = input("Perform :\n(M)erge sort\n(B)ubble sort\n(Q)uit\n").lower()
+    print("\n>>>Perform :\n(M)erge sort\n(B)ubble sort\n(Q)uit")
+    ch = input("\nEnter your choice: ").lower()
     
     if ch == 'm':
-        mergeSort(numList)
+        print(mergeSort(numList))
 
     elif ch == 'b':
         print(bubbleSort(numList))
