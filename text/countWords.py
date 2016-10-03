c36b64c08a1e76f09121703bdccbd34c44686b1c
Jatin K Malik <jk.malik@yahoo.com>
2016-10-03 16:09:29 +0000

    Word summary generator - Init

---
+++ text/countWords.py
@@ -0,0 +1,12 @@
+# Count words in a string
+# Counts the number of individual words in a string. For added complexity read these strings in from a text file and generate a summary.
+
+def wordSummary(str):
+    # Do the magic here
+
+print("\n\t\tWord counter!\n")
+str = input("Enter a string to generate word summary: ")
+
+wordSummary(str)
+
+
