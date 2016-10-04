acd352f2f47ee2e5985f94132f40fe7d11e1c3a3
Jatin K Malik <jk.malik@yahoo.com>
2016-10-04 11:35:02 +0000

    Implement word count in a string and show summary

--- text/countWords.py
+++ text/countWords.py
@@ -2,11 +2,21 @@
 # Counts the number of individual words in a string. For added complexity read these strings in from a text file and generate a summary.
 
 def wordSummary(str):
-    # Do the magic here
+    wordDict = {}
+    initVal = 1
+    for word in str.split():
+        try: # to check if word already exists in the dictionary
+            wordDict[word] += 1
+            continue
+        except KeyError: # to add a new word in dictionary
+            wordDict[word] = initVal
+    print("\n>>> Word summary:\n\n",wordDict,"\n")
 
 print("\n\t\tWord counter!\n")
 str = input("Enter a string to generate word summary: ")
-
 wordSummary(str)
 
+# To do:
+# Print dictionary in descending order
+# Read from a file
 
