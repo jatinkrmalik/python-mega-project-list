ba1ac97f381bb16892ea1b109413b801b2312d0c
Jatin K Malik <jk.malik@yahoo.com>
2016-10-04 12:13:37 +0000

    Defining methods/structure for message crypter

---
+++ text/cryptMe.py
@@ -0,0 +1,28 @@
+# Vigenere / Vernam / Ceasar Ciphers
+# Functions for encrypting and decrypting data messages. Then send them to a friend.
+
+def vigenere(str,mode):
+    # Magic here
+
+def vernam(str,mode):
+    # Magic here
+
+def ceasar(str,mode):
+    # Magic here
+
+print("\n\t\tSecret messenger\n")
+ch = input("(E)ncrypt my message\n(D)ecrypt my message\n(Q)uit\nEnter your choice: ")
+
+ch = ch.lower()
+
+if ch == 'e':
+    str = input("Enter your message: ")
+    op = int(input("What type of cipher would you like to create by encrypting your message?\n1. Vigenere cipher\n2. Vernam cipher\n3. Ceasar cipher\nEnter your choice: "))
+    
+    if op == 1:
+        
+
+elif ch == 'd':
+
+
+
