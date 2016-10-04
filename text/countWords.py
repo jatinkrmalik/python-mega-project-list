b92ba25963989f34427440b99bcef708ba4a3eca
Jatin K Malik <jk.malik@yahoo.com>
2016-10-04 11:49:09 +0000

    Implementing sorted output in countWords.py

--- text/countWords.py
+++ text/countWords.py
@@ -1,6 +1,8 @@
 # Count words in a string
 # Counts the number of individual words in a string. For added complexity read these strings in from a text file and generate a summary.
 
+import operator # to sort the dictionary
+
 def wordSummary(str):
     wordDict = {}
     initVal = 1
@@ -10,13 +12,17 @@ def wordSummary(str):
             continue
         except KeyError: # to add a new word in dictionary
             wordDict[word] = initVal
-    print("\n>>> Word summary:\n\n",wordDict,"\n")
+    wordDict = sorted(wordDict.items(), key=operator.itemgetter(1), reverse=True) # sorting the dictionary using operator lib and converting it to a tuple
+
+    print("\n>>> Word summary:\n")
+    for word in wordDict: # pretty print
+        print(word[1],"count(s)\t: ", word[0])
 
 print("\n\t\tWord counter!\n")
 str = input("Enter a string to generate word summary: ")
 wordSummary(str)
+print() # asthetics
 
 # To do:
-# Print dictionary in descending order
 # Read from a file
 
