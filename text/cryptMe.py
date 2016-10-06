6a80a16515392e54e62f41a4709aa3b4e23b416d
Jatin K Malik <jk.malik@yahoo.com>
2016-10-06 11:04:34 +0000

    Adding Brute force method for ceaser decryption

--- text/cryptMe.py
+++ text/cryptMe.py
@@ -8,72 +8,85 @@ for i in range(26):
     alphaList.append(chr(j)) # populating list with alphabets
     j += 1
 
-"""def vigenere(str,mode):
+"""def vigenere(msg,mode):
     if mode == "e": # encryption
 
     elif mode == "d": # decryption
 
-def vernam(str,mode):
+def vernam(msg,mode):
     if mode == "e": # encryption
 
     elif mode == "d": # decryption
 """
 
 
-def ceasar(str,mode): # To imporve the acceptance of both upper and lower case
+def ceasar(msg,mode): # To imporve the acceptance of both upper and lower case
     op = ""
     if mode == "e": # encryption
-        key = int(input("Enter the key for ceasar encryption: "))
-        for i in range(len(str)):
-            if ord(str[i])>64 and ord(str[i])<91:
-                op = op+alphaList[int(alphaList.index(str[i])+key)%26]
+        key = int(input("\n>>>Enter the key for ceasar encryption: "))
+        for i in range(len(msg)):
+            if ord(msg[i])>64 and ord(msg[i])<91:
+                op = op+alphaList[int(alphaList.index(msg[i])+key)%26]
             else:
-                op = op+str[i]
+                op = op+msg[i]
         print("\nCeaser cipher for your message = ",op)
         
     elif mode == "d": # decryption
-        key = int(input("Enter the key to decrypt the ceasar cipher: "))
-        for i in range(len(str)):
-            if ord(str[i])>64 and ord(str[i])<91:
-                op = op+alphaList[int(alphaList.index(str[i])-key)%26]
-            else:
-                op = op+str[i]
-        print("\nCeaser decryption for your message = ",op)
+        ch = input("\nDo you want to decrypt using :\n(K)ey\n(B)rute force method\n>>>Enter your choice: ")
+        if ch.lower() == "k":
+            key = int(input("Enter the key to decrypt the ceasar cipher: "))
+            for i in range(len(msg)):
+                if ord(msg[i])>64 and ord(msg[i])<91:
+                    op = op+alphaList[int(alphaList.index(msg[i])-key)%26]
+                else:
+                    op = op+msg[i]
+            print("\nCeaser decryption for your message = ",op)
 
+        else:
+            print("\nAttemping Brute force on:\n>>>",msg,end="\n")
+            for key in range(26):
+                op = ""
+                for i in range(len(msg)):
+                    if ord(msg[i])>64 and ord(msg[i])<91:
+                        op = op+alphaList[int(alphaList.index(msg[i])-key)%26]
+                    else:
+                        op = op+msg[i]
+
+                print("\nKey = "+str(key)+"\nMessage = ", op)
 
 print("\n\t\tSecret messenger\n")
 ch = input("(E)ncrypt my message\n(D)ecrypt my message\n(Q)uit\n\n>>>Enter your choice: ")
 ch = ch.lower()
 
 if ch == 'e':
-    str = input("\n>>> Enter your message to encrpyt: ")
-    str = str.upper() # to reduce the complexity
+    msg = input("\n>>> Enter your message to encrpyt: ")
+    msg = msg.upper() # to reduce the complexity
     op = int(input("\nWhat type of cipher would you like to create by encrypting your message?\n1. Vigenere cipher\n2. Vernam cipher\n3. Ceasar cipher\n\n>>>Enter your choice: "))
     
     if op == 1:
-       vigenere(str, "e")
+       vigenere(msg, "e")
 
     elif op == 2:
-        vernam(str, "e")
+        vernam(msg, "e")
 
     elif op == 3:
-        ceasar(str, "e")
+        ceasar(msg, "e")
     
     else:
 	    print("Wrong choice entered!")
 
 elif ch == 'd':
-    str = input("\nEnter your encrypted cipher to decrypt: ").upper()
+    msg = input("\nEnter your encrypted cipher to decrypt: ").upper()
     op = int(input("\nWhat type of cipher do you have to decrypt your message?\n1. Vigenere cipher\n2. Vernam cipher\n3. Ceasar cipher\n\n>>>Enter your choice: "))
 
     if op == 1:
-        vigenere(str, "d")
+        vigenere(msg, "d")
 
     elif op == 2:
-        vernam(str, "d")
+        vernam(msg, "d")
 
     elif op == 3:
-        ceasar(str, "d")
+        ceasar(msg, "d")
 	
     else:
         print("Wrong choice entered!")
