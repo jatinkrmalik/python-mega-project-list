b03fc049125c2eec539babeb22da87a8dfe6bb24
Jatin K Malik <jk.malik@yahoo.com>
2016-10-09 10:03:09 +0000

    Adding Regex query tool

---
+++ text/regexIt.py
@@ -0,0 +1,17 @@
+# Regex Query Tool
+# A tool that allows the user to enter a text string and then in a separate control enter a regex pattern. It will run the regular expression against the source text and return any matches or flag errors in the regular expression.
+
+import re # for ease of regex
+
+def regexQuery(txtStr, ptrn):
+    result = re.search(ptrn, txtStr)
+    print(result)
+
+
+print("\n\t\tRegex query tool")
+txtStr = input("\nEnter your text string: ")
+ptrn = input("\nEnter your regex pattern: ")
+
+regexQuery(txtStr, ptrn)
+
+
