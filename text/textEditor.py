fb08aa015384391eef491ff1ec386b328e767b88
Jatin K Malik <jk.malik@yahoo.com>
2016-10-12 13:07:59 +0000

    Added multiline input, text as WYSIWYG

--- text/textEditor.py
+++ text/textEditor.py
@@ -1,12 +1,11 @@
 # Text editor
-# Notepad style application that can open, edit, and save text documents. Optional: Add syntax highlighting and other features.
+# Notepad style application that can open, edit, and save text documents. 
+# Optional: Add syntax highlighting and other features.
 
 import os
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
-
-print("\n\t\tText Editor\n")
 
 try: # to check if directory exists
     os.mkdir("documents")
@@ -14,42 +13,54 @@ try: # to check if directory exists
 except FileExistsError:
     os.chdir("documents")
 
-ch = input("\n-------\n(O)pen a new file\n(E)dit an exiting file\n(Q)uit\n>>>Enter your choice: ").lower()
+while True:
+    print("\n\t\tText Editor")
 
-if ch == "o":
-    cls() # to clear the screen
-    plainText = input("Enter your text below:\n\n")
-    _ch = input("\n--------\n(S)ave file\t(Q)uit without saving\n>>>Enter your choice: ").lower()
+    ch = input("\n(O)pen a new file\n(E)dit an exiting file\n(Q)uit\n\n>>>Enter your choice: ").lower()
+
+    if ch == "o":
+        cls() # to clear the screen
+        print("Enter your text below (Press Ctrl+C to escape from editor):\n--------\n")
     
-    if _ch == "s":
-        fileName = input("Enter your file name: ")
-        fo = open(fileName+".txt", "wb")
-        fo.write(bytes(plainText, 'UTF-8'))
-        fo.close()
+        try:
+            textPara = ""
+            while True:
+                textLine = input()
+                textPara = textPara+"\n"+textLine
+        except KeyboardInterrupt:
+            pass
 
-    elif _ch == "q":
-        exit()
+        _ch = input("\n--------\n\n(S)ave file\t(Q)uit without saving\n\n>>>Enter your choice: ").lower()
+    
+        if _ch == "s":
+            fileName = input("Enter your file name: ")
+            fo = open(fileName+".txt", "w+")
+            fo.write(textPara)
+            fo.close()
 
-if ch == "e":
-    fileName = input("Enter the name of file you want to open: ")
-    fo = open(fileName+".txt", "rb")
-    cls()
-    print("Content of "+fileName+" is:\n--------\n")
-    print(fo.read())
-    fo.close()
-    _ch = input("\n--------\n(E)dit this file\t(Q)uit\n>>>Enter your choice: ").lower()
+        elif _ch == "q":
+            exit()
 
-    if _ch == "e":
-        plainText = input("Enter the text you want to add below:\n\n")
-        fo = open(fileName+".txt", "ab+")
-        fo.write(bytes(" "+plainText, 'UTF-8'))
-        print("\nUpdated content of file is: \n")
-        fo.seek(0)
+    if ch == "e":
+        fileName = input("Enter the name of file you want to open: ")
+        fo = open(fileName+".txt", "r+")
+        cls()
+        print("Content of "+fileName+" is:\n--------")
         print(fo.read())
         fo.close()
+        _ch = input("\n--------\n\n(A)dd more text to this file\t(Q)uit\n\n>>>Enter your choice: ").lower()
 
-    elif _ch == "q":
+        if  _ch == "a":
+            plainText = input("Enter the text you want to add below:\n")
+            fo = open(fileName+".txt", "a+")
+            fo.write("\n"+plainText)
+            print("\nUpdated content of file is:\n--------")
+            fo.seek(0)
+            print(fo.read())
+            fo.close()
+
+        elif _ch == "q":
+            exit()
+
+    elif ch == "q":
         exit()
-
-elif ch == "q":
-    exit()
