75cc4781107818744a495cbbbcae9f2a93beb36f
Jatin K Malik <jk.malik@yahoo.com>
2016-10-14 18:28:07 +0000

    Defining functions for Unit converter

--- numbers/unitConverter.py
+++ numbers/unitConverter.py
@@ -1,10 +1,36 @@
 # Unit Converter
 # Converts various units between one another. The user enters the type of unit being entered, the type of unit they want to convert to and then the value. The program will then make the conversion.
 
+import os
+
 def cls(): # to clear the screen
         os.system('cls' if os.name=='nt' else 'clear')
 
+# Defining functions
+
+def tempConv():
+    cls()
+    print("\n\tTemperature Converter\n")
+
+def lenConv():
+    cls()
+    print("\n\tLength Converter\n")
+
+def areaConv():
+    cls()
+    print("\n\tArea Converter\n")
+
+def volConv():
+    cls()
+    print("\n\tVolume Converter\n")
+
+def currConv():
+    cls()
+    print("\n\tCurrency Converter\n")
+
+
 while(True):
+    cls()
     print("\n\t\tWelcome to Unit converter\n")
     ch = input("(T)emperature\n(L)ength\n(A)rea\n(V)olume\n(C)urrency\n(Q)uit\n\n>>Enter your choice: ").lower()
 
@@ -25,8 +51,11 @@ while(True):
 
     elif ch == "q":
         break
+    
+    else:
+        print("\nWrong choice entered, please try again!")
 
-
+    input("\nPress enter to continue...")
 
     
 
