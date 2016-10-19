22a0c28b4d4ed1b3d4bcb1ab5f17877b8157854c
Jatin K Malik <jk.malik@yahoo.com>
2016-10-19 10:10:33 +0000

    Updating atomic clock to update in real time

--- web/atomicTime.py
+++ web/atomicTime.py
@@ -1,13 +1,17 @@
 # Atomic time
 # This program will get the true atomic time from an atomic time clock on the Internet.
 
+import os
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 
-page = urlopen("https://www.timeanddate.com/worldclock/fullscreen.html?n=4757") # timeanddate.com allows non-commercial scrapping so..
+def cls(): # to clear the screen
+    os.system('cls' if os.name=='nt' else 'clear')
 
-soup = bs(page, "html5lib")
+while(True): # to update in real time
+    page = urlopen("https://www.timeanddate.com/worldclock/fullscreen.html?n=4757") # timeanddate.com allows non-commercial scrapping so..
+    soup = bs(page, "html5lib") # creating a beautiful soup object
+    atomicTime =  soup.select("#i_time")[0].get_text() # extracting atomic time from page
+    cls()
+    print("\n>>>Atomic time UTC : ",atomicTime,"\n")
 
-atomicTime =  soup.select("#i_time")[0].get_text()
-
-print("\nAtomic time UTC : ",atomicTime,"\n")
