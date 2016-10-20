ff44e80ac21e4ae437c45c546889b2717fcf8ad3
Jatin K Malik <jk.malik@yahoo.com>
2016-10-20 10:15:47 +0000

    Modularising the program for weather by current location or pin code

--- web/weather.py
+++ web/weather.py
@@ -6,16 +6,25 @@ import json
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 
-print("Fetching location...")
-send_url = 'http://freegeoip.net/json'
-r = requests.get(send_url)
-j = json.loads(r.text)
-lat = str(j['latitude'])
-lon = str(j['longitude'])
-print("Getting weather for Latitude:",lat,"and Longitude",lon,"...")
+def getLoc():
+    send_url = 'http://freegeoip.net/json'
+    r = requests.get(send_url)
+    j = json.loads(r.text)
+    lat = str(j['latitude'])
+    lon = str(j['longitude'])
+    return lat,lon
 
-wPage = urlopen("https://weather.com/en-IN/weather/today/l/"+lat+","+lon)
+print("\n\t\tWeather Channel!\n")
+ch = input("Get me weather for:\n(C)urrent location\n(P)in code\n\n>>Enter your choice: ").lower()
 
-soup = bs(wPage, "html5lib")
+if ch == 'c':
+    print("Fetching location...")
+    lat,lon = getLoc()
+    print("Getting weather for Latitude:",lat,"and Longitude",lon,"...")
+    wPage = urlopen("https://weather.com/en-IN/weather/today/l/"+lat+","+lon)
+    soup = bs(wPage, "html5lib")
+    print(soup.prettify())
 
-print(soup.prettify())
+elif ch == 'p':
+    pin = input("Enter the pin code :")
+    # magic here
