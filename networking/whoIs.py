b79fca28fb1fb2972591ace7045dbbef35fb0c1c
Jatin K Malik <jk.malik@yahoo.com>
2016-10-25 08:51:59 +0000

    Part-implementing whoisDomain()

--- networking/whoIs.py
+++ networking/whoIs.py
@@ -2,6 +2,9 @@
 # Enter an IP or host address and have it look it up through whois and return the results to you.
 
 import os
+import requests
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen
 
 def cls(): # to clear the screen
     os.system('cls' if os.name=='nt' else 'clear')
@@ -11,8 +14,29 @@ def cls(): # to clear the screen
 
 def whoisDom(domain):
     print("Fetching information for",domain)
-    # do magic here
-    return 0
+    domPage = urlopen("https://who.is/whois/"+str(domain))
+    soup = bs(domPage, "html5lib")
+
+    print("\n# Registrar Info:")
+    key = soup.select(".queryResponseBodyKey")
+    val = soup.select(".queryResponseBodyValue")
+
+    for i in range(4):
+        print(">>>",key[i].get_text(), end=":")
+        print(val[i].get_text())
+
+    print("\n# Important Dates:")
+    
+    for i in range(4,7):
+        print(">>>",key[i].get_text(), end=":")
+        print(val[i].get_text())
+
+    print("\n# Name Servers:")
+    print(">>>",val[7].get_text(),":", val[8].get_text())
+    print(">>>",val[9].get_text(),":", val[10].get_text())
+    
+    # to continue x = $(".queryResponseBodyValue") in console and check index for Registrar Data
+`   return 0
 
 def whoisIP(ipadr):
     print("Fetching information for",ipadr)
