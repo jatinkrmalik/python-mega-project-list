c31f1627f104bf83118a1eeaee65d795a8a03fd8
Jatin K Malik <jk.malik@yahoo.com>
2017-01-25 10:11:40 +0000

    Initial version of Website downtime scanner with time interval

---
+++ networking/isMyWebsiteDown.py
@@ -0,0 +1,29 @@
+# Site Checker with Time Scheduling
+# An application that attempts to connect to a website or server every so many minutes or a given time and check if it is up. If it is down, it will notify you by email or by posting a notice on screen.
+
+
+# Use whiptail for notice on screen: 
+# Example: whiptail --title "Example Dialog" --msgbox "This is an example of a message box. You must hit OK to continue." 8 78
+
+import os
+import time
+from urllib.request import urlopen
+from urllib.error import URLError, HTTPError # for error handling
+
+
+def checkSite(weburl, timeInt):
+    while(True):
+        try:
+            status = urlopen(weburl).getcode()
+        except URLError:
+            os.system("whiptail --title 'Website is DOWN!' --msgbox 'Press Ok to continue' 8 78")
+        else:
+            print("Website is UP! - "+str(status))
+        
+        time.sleep(timeInt)
+
+print("\t\tSite checker with Time Scheduling\n")
+weburl = input("Enter the website url you want to check (http://example.com): ")
+timeInt = int(input("Enter the Time interval in seconds between each connection attempt: "))
+
+checkSite(weburl, timeInt)
